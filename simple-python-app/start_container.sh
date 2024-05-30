f4c1ca3774c2bb7f38b902c44b51844c0b6d306a
abhi4556 <abhijit.myworld@gmail.com>
2024-05-30 20:23:38 +0530

    Added start_container

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull abhimukh/simple-python-flask-app:latest
+echo "Pulling image"
 
 # Run the Docker image as a container
-echo
+echo "Running docker"
+docker run -d -p 5000:5000 abhimukh/simple-python-flask-app:latest
